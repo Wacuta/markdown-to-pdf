8c80dc6c627d3c1c589a2595df5920873d1b0d7f
Maxime Guyot <maxguy45@gmail.com>
2020-05-30 00:17:07 +0200

    First version of project
    This version fulfills the main use case:
     - convert markdown file to pdf file
     - keep the input organisation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# markdown-to-pdf
-File converter from Markdown to PDF
+# Markdown to PDF
+
+Node project that converts Markdown `.md` files to PDFs, keeping the input organisation.
+
+This project uses [markdown-pdf](https://github.com/alanshaw/markdown-pdf) dependency.
+
+> Your Markdown is first converted to HTML, then pushed into the HTML5 Boilerplate index.html. Phantomjs renders the page and saves it to a PDF.
+
+## Pre-requisite
+
+To run the program, you will need [Node.js](https://nodejs.org/)
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+Put your markdown files in `/input` folder and launch the programme.
+
+```sh
+node index.js
+```
+
+You will find the result in the `/output` folder.
+
+## Contribute
+
+Do not hesitate! [Open an issue](https://github.com/Wacuta/markdown-to-pdf/issues/new) or submit Pull Requests.
+
+## License
+
+[MIT](LICENCE) © Maxime Guyot
---
+++ index.js
@@ -0,0 +1,50 @@
+﻿let markdownpdf = require("markdown-pdf")
+var fs = require('fs');
+var path = require('path');
+
+const INPUT_FOLDER = './input'
+const OUTPUT_FOLDER = './output'
+
+const REGEX_MD = /^[-'a-zA-ZÀ-ÖØ-öø-ÿ]*.(md|MD)$/;
+const REGEX_FILE = /^[-'a-zA-ZÀ-ÖØ-öø-ÿ]*[^.]\w*$/;
+
+
+/* Parcour en profondeur su ce n'est pas un .md */
+let searchMarkdownFiles = function(dir, done) {
+    fs.readdir(dir, function(err, files) {
+        if(err) return done(err); // erreur de dossier
+
+        files.forEach(file => {
+            
+            let tmp = dir+'/'+file
+
+            if (REGEX_FILE.test(file)) {
+                getMD(tmp, function(err, results) {
+                    if (err) throw err;
+                    done(null, results)
+                })
+            }
+
+            if (REGEX_MD.test(file)) {
+                done(null, tmp)
+            }
+
+        });
+    })
+}
+
+searchMarkdownFiles(INPUT_FOLDER, function(err, results) {
+    if (err) throw err;
+
+    let outputPdf = results
+        .replace(INPUT_FOLDER, OUTPUT_FOLDER)
+        .replace('.md', '.pdf')
+        .replace('.MD', '.pdf')
+
+    markdownpdf()
+        .from(results)
+        .to(outputPdf, function () {
+            console.log(outputPdf + " created")
+        })
+});
+
